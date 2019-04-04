7ba181f7ed7b5ea4c2aa9cde48c0b2bbcee9f7a0
kitaye <44532933+kitaye@users.noreply.github.com>
2019-04-04 21:53:28 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 function activeTab() {
 	const tabs = document.querySelectorAll(".tabs__li");
-		for (let i = 0; i < tabs.length; i++) {
-			tabs[i].addEventListener("click", function() {
-				if (tabs[i] )
-				tabs[i].classList.add("selected-tab");
+	const sections = document.querySelectorAll(".section");
+	const tabActiveClass = "selected-tab";
+	const sectionActiveClass = "section--active";
+
+	// for (let i = 0; i < tabs.length; i++) 
+	for (const tab of tabs) {
+		tab.addEventListener("click", function() {
+			if (!(this.classList.contains(tabActiveClass))) {
+				for (const iter of tabs) {
+					iter.classList.remove(tabActiveClass);
+				}
+				this.classList.add(tabActiveClass);
+			} 
+
+			const value = tab.dataset.tab;
+			console.log(value);
+			const section = document.querySelector(`[data-section='${value}']`);
+			console.log(section);
+			for (const item of sections) {
+				item.classList.remove(sectionActiveClass);
+			}
+			section.classList.add(sectionActiveClass);
 		});
 	}
 }		
+
+activeTab();
+
+
+
+
 		
